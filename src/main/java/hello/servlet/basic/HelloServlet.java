14e3761bd844fec5c4afd06e132fd3f47628c747
thgus980 <thgus980@naver.com>
2025-04-09 22:03:07 +0900

    test

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello") // urlPatterns에 해당하는 url로 접속하면 이게 실행 되는 것
 public class HelloServlet extends HttpServlet {
-    @Override //서블릿이 호출이 되면 service 메소드가 호출된다
+    @Override //서블릿이 호출이 되면 service 메소드가 호출된다.
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
     }
