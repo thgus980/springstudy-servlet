fda584c2fb6ca84d3531bebf5ffd26a88a2bd80b
thgus980 <thgus980@naver.com>
2025-04-10 18:29:57 +0900

    20250410 학습내용

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -11,7 +11,17 @@ import java.io.IOException;
 @WebServlet(name = "helloServlet", urlPatterns = "/hello") // urlPatterns에 해당하는 url로 접속하면 이게 실행 되는 것
 public class HelloServlet extends HttpServlet {
     @Override //서블릿이 호출이 되면 service 메소드가 호출된다.
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
+        System.out.println("request = " + request); //request = org.apache.catalina.connector.RequestFacade@456c96c6
+        System.out.println("response = " + response); //response = org.apache.catalina.connector.ResponseFacade@4fb1e906
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello"+username);
+
     }
 }
